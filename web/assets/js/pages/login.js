b14a17f5522ac1eddad06ce01a8ad3ec1c66b541
dongyang <dong.yang@mail.dealmoon.com>
2018-11-20 14:53:11 +0800

    assets

---
+++ web/assets/js/pages/login.js
@@ -0,0 +1,72 @@
+//------------- login.js -------------//
+$(document).ready(function() {
+
+	//for custom checkboxes
+	$('input').not('.noStyle').iCheck({
+        checkboxClass: 'icheckbox_flat-green'
+    });
+
+	//validate login form 
+	$("#login-form").validate({
+		ignore: null,
+		ignore: 'input[type="hidden"]',
+		errorPlacement: function(error, element) {
+			wrap = element.parent();
+			wrap1 = wrap.parent();
+			if (wrap1.hasClass('checkbox')) {
+				error.insertAfter(wrap1);
+			} else {
+				if (element.attr('type')=='file') {
+					error.insertAfter(element.next());
+				} else {
+					error.insertAfter(element);
+				}
+			}
+		}, 
+		errorClass: 'help-block',
+		rules: {
+			email: {
+				required: true,
+				email: true
+			},
+			password: {
+				required: true,
+			}
+		},
+		messages: {
+			password: {
+				required: "请输入密码",
+			},
+			email: {
+                required: "请输入邮箱",
+                email: "邮箱格式错误",
+            }
+		},
+		highlight: function(element) {
+			if ($(element).offsetParent().parent().hasClass('form-group')) {
+				$(element).offsetParent().parent().removeClass('has-success').addClass('has-error');
+			} else {
+				if ($(element).attr('type')=='file') {
+					$(element).parent().parent().removeClass('has-success').addClass('has-error');
+				}
+				$(element).offsetParent().parent().parent().parent().removeClass('has-success').addClass('has-error');
+				
+			}
+	    },
+	    unhighlight: function(element,errorClass) {
+	    	if ($(element).offsetParent().parent().hasClass('form-group')) {
+	    		$(element).offsetParent().parent().removeClass('has-error').addClass('has-success');
+		    	$(element.form).find("label[for=" + element.id + "]").removeClass(errorClass);
+	    	} else if ($(element).offsetParent().parent().hasClass('checkbox')) {
+	    		$(element).offsetParent().parent().parent().parent().removeClass('has-error').addClass('has-success');
+	    		$(element.form).find("label[for=" + element.id + "]").removeClass(errorClass);
+	    	} else if ($(element).next().hasClass('bootstrap-filestyle')) {
+	    		$(element).parent().parent().removeClass('has-error').addClass('has-success');
+	    	}
+	    	else {
+	    		$(element).offsetParent().parent().parent().removeClass('has-error').addClass('has-success');
+	    	}
+		}
+	});
+
+});
